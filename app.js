88acfb1bdd0bd0180d77b8f3c12c8e773cc57810
Rebecca Winterfield <rebeccawinterfield@Rebeccas-MacBook-Pro.local>
2018-01-01 22:22:54 -0500

    rock

--- app.js
+++ app.js
@@ -8,28 +8,28 @@ var path = require('path')
 
 var app = express();
 
-// const forceSSL = function () {
-//     return function (req, res, next) {
-//         if (req.headers['x-forwarded-proto'] !== 'https') {
-//             return res.redirect(
-//                 ['https://', req.get('Host'), req.url].join('')
-//             );
-//         }
-//         next();
-//     }
-// }
-// // Instruct the app
-// // to use the forceSSL
-// // middleware
-// app.use(forceSSL());
+const forceSSL = function () {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//     next();
+// });
 
 // configuration ===========================================
 var smtpTransport = nodemailer.createTransport({
