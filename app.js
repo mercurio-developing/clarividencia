e04feb5b2756f99ba952cd04def40bbe0cc12fd2
Rebecca Winterfield <rebeccawinterfield@Rebeccas-MacBook-Pro.local>
2018-01-01 21:16:56 -0500

    rock

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, token, authorization');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
-})
+});
 
 // configuration ===========================================
 var smtpTransport = nodemailer.createTransport({
