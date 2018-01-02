c74adf5c8d32c0c3eedd116bb94c8cb3d99549c2
Rebecca Winterfield <rebeccawinterfield@Rebeccas-MacBook-Pro.local>
2018-01-01 22:36:36 -0500

    rock

--- app.js
+++ app.js
@@ -8,20 +8,6 @@ var path = require('path')
 
 var app = express();
 
-const forceSSL = function () {
-    return function (req, res, next) {
-        if (req.headers['x-forwarded-proto'] !== 'https') {
-            return res.redirect(
-                ['https://', req.get('Host'), req.url].join('')
-            );
-        }
-        next();
-    }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
 
 // app.use(function (req, res, next) {
 //     res.header("Access-Control-Allow-Origin", '*');
--- src/app/components/contact/contact.service.ts
+++ src/app/components/contact/contact.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http,Headers,RequestOptions } from '@angular/http';
 
 @Injectable()
 export class ContactService {
 
-  constructor(private http:Http) { }
+  constructor(private http:Http) {
+
+
+
+   }
+
 
   sendEmail(dataEmail) {
     console.log(dataEmail)
-    this.http.post('http://localhost:3000/sendmail', dataEmail).subscribe((data) => {
+    const headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+    const options = new RequestOptions({ headers: headers });  
+    this.http.post('http://localhost:3000/sendmail', dataEmail,options).subscribe((data) => {
           console.log(data)
             console.log('the email is sended')
         })
