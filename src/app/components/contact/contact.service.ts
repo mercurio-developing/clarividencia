2d86897edbdfadf613d2eda85d9019581cb9213e
Rebecca Winterfield <rebeccawinterfield@Rebeccas-MacBook-Pro.local>
2018-01-06 16:17:53 -0500

    as

--- src/app/components/contact/contact.service.ts
+++ src/app/components/contact/contact.service.ts
@@ -11,10 +11,6 @@ export class ContactService {
 
   sendEmail(dataEmail) {
     console.log(dataEmail)
-    // const headers = new Headers();
-    // headers.append('Accept', 'application/json');
-    // headers.append('Content-Type', 'application/json');
-    // const options = new RequestOptions({ headers: headers });  
     this.http.post('https://clarividencia-fotografia.herokuapp.com:443/sendmail', dataEmail).subscribe((data) => {
           console.log(data)
             console.log('the email is sended')
